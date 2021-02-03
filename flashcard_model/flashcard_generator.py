d6317024305a2e8b166cdd8a29bf05ecfc34f4fd
Tinu Theckel Joy <tinutjoy88@gmail.com>
2021-02-03 16:52:48 +1100

    reposition the assert statement

--- flashcard_model/flashcard_generator.py
+++ flashcard_model/flashcard_generator.py
@@ -64,11 +64,11 @@ class FlashCardGenerator:
         """
         if self.model is None:
             self.model = self.load_model(self.model_name)
+            assert self.model is not None, f"Issues with loading pretrained {self.model_name} model"
             self.model.to(self.device)
         if self.text_processor is None:
             self.text_processor = TextPreprocessor(self.tokenizer_name)
             self.text_processor._initialize_tokenizer()
-        assert self.model is not None, f"Issues with loading pretrained {self.model_name} model"
 
     def generate_cards(self, text: str) -> Optional[List[FlashCard]]:
         """
